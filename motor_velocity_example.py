0fd618fcafeec0e206aee5ee6b67bca225f9f18d
BaronKhan <bksonic@hotmail.co.uk>
2016-11-02 12:51:18 +0000

    Fix tuning

--- motor_velocity_example.py
+++ motor_velocity_example.py
@@ -11,16 +11,16 @@ interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
 
 motorParams = interface.MotorAngleControllerParameters()
-motorParams.maxRotationAcceleration = 6.0
+motorParams.maxRotationAcceleration = 4.0
 motorParams.maxRotationSpeed = 12.0
 motorParams.feedForwardGain = 255/20.0
-motorParams.minPWM = 5.0
+motorParams.minPWM = 1.0
 motorParams.pidParameters.minOutput = -255
 motorParams.pidParameters.maxOutput = 255
 # 517, 1000, 13
-motorParams.pidParameters.k_p = 150
-motorParams.pidParameters.k_i = 20
-motorParams.pidParameters.k_d = 10
+motorParams.pidParameters.k_p = 100
+motorParams.pidParameters.k_i = 0
+motorParams.pidParameters.k_d = 0
 
 
 interface.setMotorAngleControllerParameters(motors[0],motorParams)
